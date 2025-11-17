5451bb85bbd504af9b1c3f03ef8bcd5b3693c5d8
Rone <Gamewarrior1andbowser@gmail.com>
2025-11-17 13:40:01 -0600

    Made JS able to edit detail.HTML and updated Credits.txt

--- assets/script/interaction.js
+++ assets/script/interaction.js
@@ -1,5 +1,20 @@
+//const brandName = ["text", "img", "img", "img"];
+let brand = "";
 const login = document.getElementById("login");
 const popup = document.getElementById("popup");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const text = document.getElementById("text");
+
+
+if (brand == "") {
+  text.value = brandName[0];
+  img1.value = brandName[1];
+  img2.value = brandName[2];
+  img3.value = brandName[3];
+}
+
 
 popup.addEventListener('click', function() {
   profile.classList.toggle('visible');
