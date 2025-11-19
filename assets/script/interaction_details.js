26419d8929484966a3cf02ece2af98fc02a0f82e
Rone <Gamewarrior1andbowser@gmail.com>
2025-11-19 15:35:54 -0600

    finshed JS for interaction_details

--- assets/script/interaction_details.js
+++ assets/script/interaction_details.js
@@ -1,12 +1,13 @@
-const AeroFlow = ["Stride AeroFlow Running Shoe", "$129.99", "The Stride AeroFlow is engineered for comfort, speed, and durability. Featuring breathable mesh fabric, ultra-light cushioning, and a supportive sole — perfect for running, training, or everyday wear. It fits naturally around your foot, giving you a light and comfortable feel from the moment you put it on. The mesh design allows plenty of airflow, so your feet stay cool and dry even during long runs or workouts. It’s built to support you through any activity without feeling heavy.", "../media/shoe.jpg"];
-const alt = ["(Name)", "(Price)", "(Description)", "../media/stride-logo.png"];
+const AeroFlow = ["Stride AeroFlow Running Shoe", "$129.99", "The Stride AeroFlow marks a hige milestone for our team - The first-ever shoe designed and produced in-house by Stride. After years of curating the best footwear from top brands, we wanted to create something that truly represents who we are, fast, clean, reliable and built for everyone.", "The Stride AeroFlow is engineered for comfort, speed, and durability. Featuring breathable mesh fabric, ultra-light cushioning, and a supportive sole — perfect for running, training, or everyday wear. It fits naturally around your foot, giving you a light and comfortable feel from the moment you put it on. The mesh design allows plenty of airflow, so your feet stay cool and dry even during long runs or workouts. It’s built to support you through any activity without feeling heavy.", "./assets/media/stride-shoe.png"];
+const alt = ["(Name)", "(Price)", "(snippet)", "(description)", "./assets/media/stride-logo.png"];
 
 const product = [AeroFlow, alt];
 let index = 0;
 const txt1 = document.querySelector(".product-name");
 const txt2 = document.querySelector(".product-price");
-const txt3 = document.querySelector(".product-description");
-const img = document.querySelector(".product-img");
+const txt3 = document.querySelector(".product-snippet");
+const txt4 = document.querySelector(".product-description");
+const img = document.querySelector(".shoe-prod-img");
 const buttonLeft = document.querySelector(".left");
 const buttonRight = document.querySelector(".right");
 const indexDisplay = document.querySelector(".index");
@@ -14,10 +15,11 @@ const productDisplay = document.querySelector(".maximum");
 
 function switchProduct() {
   indexDisplay.value = String(index + 1);
-  txt1.value = product[index][0];
-  txt2.value = product[index][1];
-  txt3.value = product[index][2];
-  img.value = product[index][3];
+  txt1.innerText = product[index][0];
+  txt2.innerText = product[index][1];
+  txt3.innerText = product[index][2];
+  txt4.innerText = product[index][3];
+  img.src = product[index][4];
 }
 
 productDisplay.innerText = String(product.length);
--- assets/script/interaction_index.js
+++ assets/script/interaction_index.js
@@ -1,7 +1,7 @@
 const login = document.querySelector(".login");
 const popup = document.querySelector(".popup");
 
-
+/*
 popup.addEventListener('click', function() {
   profile.classList.toggle('visible');
 });
@@ -9,3 +9,4 @@ popup.addEventListener('click', function() {
 login.addEventListener('click', function() {
   profile.classList.toggle('visible');
 });
+*/
